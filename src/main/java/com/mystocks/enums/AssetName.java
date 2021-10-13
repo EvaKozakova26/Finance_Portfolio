8b4628c4baa13674d57e031ac5faa691a084e291
Eva Kozakova <evkozakova@seznam.cz>
2021-10-13 13:15:09 +0200

    add new shares codes

--- src/main/java/com/mystocks/enums/AssetName.java
+++ src/main/java/com/mystocks/enums/AssetName.java
@@ -13,11 +13,13 @@ public class AssetName {
 			"INTC", "Intel Corporation",
 			"TABAK.PR", "Philip Morris CR a.s.",
 			"MONET.PR", "MONETA Money Bank, a.s.",
-			"CEZ.PR", "CEZ, a. s. "
+			"CEZ.PR", "CEZ, a. s.",
+			"SPG", "Simon Property Group, Inc.",
+			"FB", "Facebook, Inc."
 	);
 
 	public static List<String> PRA_EXCHANGE = List.of("TABAK.PR", "MONET.PR", "CEZ.PR");
-	public static List<String> US_EXCHANGE = List.of("SOFI", "MCD", "SPY", "INTC");
+	public static List<String> US_EXCHANGE = List.of("SOFI", "MCD", "SPY", "INTC", "FB", "SPG");
 	public static List<String> CRYPTO_EXCHANGE = List.of("btc");
 
 }
